dccf9c6e5035281f3c7f3b5eea318bffaed59568
moas-20 <mk333hot@gmail.com>
2026-06-20 21:54:00 +0200

    select form elment using DOM

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+const title = document.querySelector("#title");
+const auther = document.querySelector("#auther");
+const pages = document.querySelector("#pages");
+// const title = document.querySelector("#title");
+
+
+// displaying form for adding new book
 const disp = document.querySelector(".form-disp");
 const form = document.querySelector(".form-container");
 disp.addEventListener("click",()=>{
     form.classList.add("active")
+});
+
+// remove the requist to add new book
+const cancleAdd = document.querySelector(".form-cns");
+cancleAdd.addEventListener('click',()=>{
+    form.classList.remove("active");
 })
+
+// adding & display new book
+const container = document.querySelector(".books-container");
+const addBook = document.querySelector(".form-add");
+addBook.addEventListener('click',()=>{
+
+})
+
+
+const myLibrary = [];
+
+function Book(title,auther,pages){
+  // the constructor...
+  this.title = title;
+  this.auther = auther;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  
+}
