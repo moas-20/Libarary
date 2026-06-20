9a8257cf1bd5d94afcbe9ce32834cd6551626f55
moas-20 <mk333hot@gmail.com>
2026-06-20 21:02:10 +0200

    adding new book functionality in html file

---
+++ script.js
@@ -0,0 +1,5 @@
+const disp = document.querySelector(".form-disp");
+const form = document.querySelector(".form-container");
+disp.addEventListener("click",()=>{
+    form.classList.add("active")
+})
