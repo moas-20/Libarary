3cdee2be53ff35f50c43d21a58b7ef0633ba1da0
moas-20 <mk333hot@gmail.com>
2026-06-21 15:58:36 +0200

    add new function addBookTLibarary & eventlistener for adding Book

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// form data
 const title = document.querySelector("#title");
 const auther = document.querySelector("#auther");
 const pages = document.querySelector("#pages");
 const readCheck = document.querySelector("#status");
-// const title = document.querySelector("#title");
+// add new book btn
+const disp = document.querySelector(".form-disp");
+const form = document.querySelector(".form-container");
+// form cancele btn
+const cancleAdd = document.querySelector(".form-cns");
+// card container
+const container = document.querySelector(".books-container");
+// form add btn
+const addBook = document.querySelector(".form-add");
 
 
 // displaying form for adding new book
-const disp = document.querySelector(".form-disp");
-const form = document.querySelector(".form-container");
 disp.addEventListener("click",()=>{
     form.classList.add("active")
 });
 
 // remove the requist to add new book
-const cancleAdd = document.querySelector(".form-cns");
 cancleAdd.addEventListener('click',()=>{
     form.classList.remove("active");
     title.value = "";
@@ -22,24 +28,27 @@ cancleAdd.addEventListener('click',()=>{
     readCheck.checked = false;
 })
 
-// adding & display new book
-const container = document.querySelector(".books-container");
-const addBook = document.querySelector(".form-add");
-addBook.addEventListener('click',()=>{
 
+// adding & display new book
+addBook.addEventListener('click',()=>{
+    addBookToLibrary();
+    
 })
 
 
 const myLibrary = [];
 
-function Book(title,auther,pages){
-  // the constructor...
-  this.title = title;
-  this.auther = auther;
-  this.pages = pages;
+function Book(title,auther,pages,status){
+    // the constructor...
+    this.title = title;
+    this.auther = auther;
+    this.pages = pages;
+    this.status = status;
 }
 
 function addBookToLibrary() {
-  // take params, create a book then store it in the array
+    // take params, create a book then store it in the array
+    let book = new Book(title.value,auther.value,pages.value,readCheck.checked);
+    myLibrary.push(book);
   
 }
